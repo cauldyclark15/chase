fcd26bc3eace04c6ff74ef57f5cb5cb7569a8498
cauldyclark15 <jcing.jc@gmail.com>
2017-10-07 21:22:39 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const FBBotFramework = require('fb-bot-framework');
+const PORT = process.env.PORT || 1515;
+
+const bot = new FBBotFramework({
+  page_token: 'EAABZBzeQXPZAoBACpC6GrAHnNPiORe2ZAR0CGBoteyL8FhIBpEZC8nQHeY6Kb8WjMgvmvkOI4nkNfFjbAuSxAjIa8thJXkwJ8MkKhVqyEcivRyXCCaDe6cnjrIP1pU7HzckfjxiUrnYZBvc4x3I8DMVvGwqOr2ZCgebIiegHsZCiAZDZD',
+  verify_token: 'x_kalawang_1'
+});
+
+app.use('/webhook', bot.middleware());
+
+bot.on('message', function (userId, message) {
+  bot.sendTextMessage(userId, `Echo Message: ${message}`);
+});
+
+app.get('/', function (req, res) {
+  res.send('hello node bot');
+});
+
+app.listen(PORT);
